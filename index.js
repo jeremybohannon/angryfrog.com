1069c5d1d0055db57de1758168e19077a5728138
JeremyBoh <jeremybohannon@outlook.com>
2016-02-01 17:01:44 -0500

    Fixed animation. added shakes

--- index.js
+++ index.js
@@ -8,24 +8,15 @@ var http = require("http");
 app.use('/', express.static(path.join(__dirname, '/views/')));
 app.use('/styles', express.static(path.join(__dirname, '/styles/')));
 app.use('/icons', express.static(path.join(__dirname, '/icons/')));
-app.use('/node_modules', express.static(path.join(__dirname, '/node_modules/csshake')));
-// app.use('*', express.static(path.join(__dirname, '/views/')));
-//
-// app.get('/', function(req, res){
-//   res.sendFile(path.join(__dirname + '/index.html'));
-// });
-// app.get('/stylesheet.css', function(req, res){
-//   res.sendFile(path.join(__dirname + '/stylesheet.css'));
-// });
+// app.use('csshake.min.css', express.static(path.join(__dirname, '/node_modules/csshake/dist/')));
 
-// app.get('/node_modules/csshake/csshake.min.css', function(req, res){
-//  res.sendFile(path.join(__dirname + '/node_modules/csshake/csshake.min.css'));
-//  });
+app.use('/styles', express.static(path.join(__dirname, '/node_modules/csshake/dist/')));
+
 
 var server = app.listen(80, function(){
 
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('AngryFrog is listening at http://%s:%s', host, port);
+  console.log('AngryFrog is listening at port %s', host, port);
 });
