95faaa064fe0b8177312fac94a68a9e168e8248d
JeremyBoh <jeremybohannon@outlook.com>
2016-02-01 16:15:17 -0500

    File rearrangement

--- index.js
+++ index.js
@@ -5,17 +5,22 @@ var path = require("path");
 var http = require("http");
 
 
-app.use('/', express.static(path.join(__dirname, '/')));
+app.use('/', express.static(path.join(__dirname, '/views/')));
 app.use('/styles', express.static(path.join(__dirname, '/styles/')));
-
-app.use('*', express.static(path.join(__dirname, '/')));
-// 
+app.use('/icons', express.static(path.join(__dirname, '/icons/')));
+app.use('/node_modules', express.static(path.join(__dirname, '/node_modules/csshake')));
+// app.use('*', express.static(path.join(__dirname, '/views/')));
+//
 // app.get('/', function(req, res){
 //   res.sendFile(path.join(__dirname + '/index.html'));
 // });
 // app.get('/stylesheet.css', function(req, res){
 //   res.sendFile(path.join(__dirname + '/stylesheet.css'));
 // });
+
+// app.get('/node_modules/csshake/csshake.min.css', function(req, res){
+//  res.sendFile(path.join(__dirname + '/node_modules/csshake/csshake.min.css'));
+//  });
 
 var server = app.listen(80, function(){
 
