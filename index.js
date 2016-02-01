a6d4c4ecbe18ef0ab0a923d33e338fabbfe66c48
JeremyBoh <jeremybohannon@outlook.com>
2016-02-01 15:52:21 -0500

    Updated website, added frogs

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ var app = express();
 var path = require("path");
 var http = require("http");
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
 
-app.get('/stylesheet.css', function(req, res){
-  res.sendFile(path.join(__dirname + '/stylesheet.css'));
-});
+app.use('/', express.static(path.join(__dirname, '/')));
+app.use('/styles', express.static(path.join(__dirname, '/styles/')));
+
+app.use('*', express.static(path.join(__dirname, '/')));
+// 
+// app.get('/', function(req, res){
+//   res.sendFile(path.join(__dirname + '/index.html'));
+// });
+// app.get('/stylesheet.css', function(req, res){
+//   res.sendFile(path.join(__dirname + '/stylesheet.css'));
+// });
 
 var server = app.listen(80, function(){
 
